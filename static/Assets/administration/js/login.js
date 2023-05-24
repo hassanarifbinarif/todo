1c9fef1626d63015f2b5f15ee01e0a30d36f2b7f
hassanarifbinarif <hassanarif2811@gmail.com>
2023-05-24 18:00:45 +0500

    UI improvements and fixes for admin panel

--- static/Assets/administration/js/login.js
+++ static/Assets/administration/js/login.js
@@ -16,3 +16,58 @@ passwordIcon.addEventListener("click", function () {
         console.log(err);
     }
 });
+
+
+// Handling Login Form
+
+async function loginForm(event) {
+    event.preventDefault();
+    let form = event.currentTarget;
+    let emailField = form.querySelector('input[name="email"]');
+    let emailMsg = form.querySelector(".email-msg");
+    let passwordField = form.querySelector('input[name="password"]');
+    let passwordMsg = form.querySelector(".password-msg");
+	let button = form.querySelector('button[type="submit"]');
+    let buttonText = button.innerText;
+
+    if (!isValidEmail(emailField)) {
+        emailField.classList.add("input-error");
+        emailMsg.classList.add("active");
+        emailField.addEventListener("input", function () {
+            if (isValidEmail(this)) {
+                this.classList.remove("input-error");
+            } else {
+                let inputField = this;
+                if (timeOut) {
+                    clearTimeout(timeOut);
+                }
+                timeOut = setTimeout(function () {
+                    inputField.classList.add("input-error");
+                    emailMsg.classList.add("active");
+                }, 1000);
+            }
+        });
+        return false;
+    } else if (!isValidPassword(passwordField)) {
+        passwordField.classList.add("input-error");
+        passwordMsg.classList.add("active");
+        passwordField.addEventListener("input", function () {
+            if (isValidPassword(this)) {
+                this.classList.remove("input-error");
+            } else {
+                let inputField = this;
+                if (timeOut) {
+                    clearTimeout(timeOut);
+                }
+                timeOut = setTimeout(function () {
+                    inputField.classList.add("input-error");
+                    passwordMsg.classList.add("active");
+                }, 1000);
+            }
+        });
+        return false;
+    }
+    else {
+        location.href = location.origin + '/administration/dashboard/';
+    }
+}
